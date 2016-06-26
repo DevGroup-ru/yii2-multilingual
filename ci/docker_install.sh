411a8b6f445c1ff561d2776bcc6178e7bfcc2e10
bethrezen <b37hr3z3n@gmail.com>
2016-06-26 12:48:43 +0300

    add unzip to dependencies

--- ci/docker_install.sh
+++ ci/docker_install.sh
@@ -7,7 +7,7 @@ set -xe
 
 # Install git (the php image doesn't have it) which is required by composer
 apt-get update -yqq
-apt-get install git -yqq
+apt-get install git unzip -yqq
 
 # Install phpunit, the tool that we will use for testing
 curl -o /usr/local/bin/phpunit https://phar.phpunit.de/phpunit.phar
